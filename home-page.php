ac6975095b516a748eb2dea7e0934df2387a6bbd
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-04-05 11:04:32 +0200

    hero scss

--- home-page.php
+++ home-page.php
@@ -4,37 +4,9 @@
  * Template Name: Home Page
  */
 
-get_header();
+get_header(); ?>
 
-$show_hero = get_field('show_hero');
-$title = get_field('hero_title');
-?>
 
-<?php if ($show_hero) : ?>
-    <section class="hero">
-        <div class="container">
-            <div class="hero__inner">
-                <?php if ($title <> '') : ?>
-                    <h1 class="hero__title"><?php echo $title ?></h1>
-                <?php endif ?>
-            </div>
-            <?php if (have_rows('hero_slider')) : ?>
-                <ul class="slides">
-                    <?php while (have_rows('hero_slider')) : the_row();
-                        $title = get_sub_field('slider_title');
-                        $image = get_sub_field('image');
-                    ?>
-                        <li>
-                            <?php if ('title' <> '') : ?>
-                                <h2 class="hero__slider_title"><?php echo $title ?>
-                                <?php endif; ?>
-                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                        </li>
-                    <?php endwhile; ?>
-                </ul>
-            <?php endif; ?>
-        </div>
-    </section>
-<?php endif ?>
+<?php get_template_part('template-parts/home-page', 'hero'); ?>
 
 <?php get_footer(); ?>
--- template-parts/home-page-hero.php
+++ template-parts/home-page-hero.php
@@ -1,7 +1,13 @@
+<?php
+$show =  get_field('show_hero');
+if (!$show)
+    return;  
+?>
+
 <section class="hero"></section>
 <div class="container">
     <div class="row">
-        <div class="col-md-6">Some 1</div>
+        <div class="col-md-6">Some 12</div>
         <div class="col-md-6">Some 2</div>
     </div>
 </div>
