81abb647c6a4e8c78ad237beb730011f8362bd6e
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-04-05 10:35:39 +0200

    hero section

---
+++ template-parts/home-page-hero.php
@@ -0,0 +1,8 @@
+<section class="hero"></section>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">Some 1</div>
+        <div class="col-md-6">Some 2</div>
+    </div>
+</div>
+</section>
