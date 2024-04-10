a0604b9b1cba43f4306439c354032f354d694ea2
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-04-10 16:15:10 +0200

    add contact- image

--- home-page.php
+++ home-page.php
@@ -8,6 +8,7 @@ get_header(); ?>
 
 
 <?php get_template_part('template-parts/home-page', 'hero'); ?>
+<?php get_template_part('template-parts/main-image', 'main-image'); ?>
 <?php get_template_part('template-parts/section-1', 'images'); ?>
 
 <?php get_footer(); ?>
--- template-parts/main-image.php
+++ template-parts/main-image.php
@@ -1,15 +1,15 @@
 <?php
-$show_image_video = get_field('show_image_video');
-if ($show_image_video) {
-    $main_image = get_field('main_image');
-    if ($main_image) {
-        $main_image_url = $main_image['url'];
-        $main_image_title = $main_image['title'];
 
-        echo '<img src="' . $main_image_url . '" alt="' . $main_image_title . '">';
-    } else {
-        echo 'Головне зображення відсутнє.';
-    }
-} else {
-    echo 'Відображення зображень та відео вимкнене.';
-}
+$show_image_video = get_field('show_image_video');
+if (!$show_image_video)
+    return;
+
+
+$main_image = get_field('main_image');
+
+?>
+<section id="main-image" class="main-image-home">
+    <?php if ($main_image <> '') : ?>
+        <img src="<?php echo $main_image['url']; ?>" alt="<?php echo $main_image['title']; ?>" class="main-image-home">
+    <?php endif; ?>
+</section>
