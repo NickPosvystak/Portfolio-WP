c012bc4434055520d778c4d5ddddef699ce0db5d
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2024-04-10 17:12:57 +0200

    title

--- template-parts/main-image.php
+++ template-parts/main-image.php
@@ -6,10 +6,23 @@ if (!$show_image_video)
 
 
 $main_image = get_field('main_image');
+$contact_me = get_field('contact_me');
+$contact_link = get_field('contact_link');
 
 ?>
 <section id="main-image" class="main-image-home">
     <?php if ($main_image <> '') : ?>
         <img src="<?php echo $main_image['url']; ?>" alt="<?php echo $main_image['title']; ?>" class="main-image-home">
     <?php endif; ?>
+    <div class="contact-content">
+
+
+        <?php if ($contact_me <> '') : ?>
+            <p class="contact-title"><?php echo $contact_me; ?></p>
+        <?php endif; ?>
+
+        <?php if ($contact_link <> '') : ?>
+            <a href="<?php echo $contact_link['url']; ?>" target="_blank" rel="noopener noreferrer" class="contact-link"><?php echo $contact_link['title']; ?></a>
+        <?php endif; ?>
+    </div>
 </section>
